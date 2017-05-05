086ad482bd41dca736ab0b8e8d7009585fd07870
Denis Yuen <denis.yuen@oicr.on.ca>
2017-05-05 17:10:35 -0400

    Fix odd star import

--- cwlavro-tools/src/main/java/io/cwl/avro/CWL.java
+++ cwlavro-tools/src/main/java/io/cwl/avro/CWL.java
@@ -23,8 +23,8 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
